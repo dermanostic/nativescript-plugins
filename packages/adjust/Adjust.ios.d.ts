f723fa43c3d3a5123408f30715d8b615baf2dc19
luke-hawk <lucas.habrich@gmx.de>
2021-08-10 20:15:44 +0200

    Added setEnabled

--- packages/adjust/Adjust.ios.d.ts
+++ packages/adjust/Adjust.ios.d.ts
@@ -1,47 +1,48 @@
 declare class ADJConfig {
-  static new(): ADJConfig; // inherited from NSObject
-  initWithAppTokenEnvironment(apiKey: string, environmentSandbox: string ): void;
-  initWithAppTokenEnvironmentAllowSuppressLogLevel(apiKey: string, environmentSandbox: string, allowSuppressLogLevel: boolean);
-  deactivateSKAdNetworkHandling(): void;
-  logLevel: string;
-  delegate: AdjustDelegate;
+	static new(): ADJConfig; // inherited from NSObject
+	initWithAppTokenEnvironment(apiKey: string, environmentSandbox: string): void;
+	initWithAppTokenEnvironmentAllowSuppressLogLevel(apiKey: string, environmentSandbox: string, allowSuppressLogLevel: boolean);
+	deactivateSKAdNetworkHandling(): void;
+	logLevel: string;
+	delegate: AdjustDelegate;
 }
 
 declare class Adjust {
-  static new(): Adjust; // inherited from NSObject
-  static alloc(): Adjust // inherited from NSObject
-  appDidLaunch(config: ADJConfig ): void;
-  requestTrackingAuthorizationWithCompletionHandler(callback: any): void;
-  addSessionCallbackParameterValue(key: string, value: string): void;
-  trackSubsessionStart(): void;
-  trackEvent(event: ADJEvent): void;
-  appWillOpenUrl(url: NSURL): void;
-  sendFirstPackages(): void;
-  setOfflineMode(isEnabled: boolean): void;
-  trackAdRevenuePayload(source: string, payload: NSData): void;
-  trackSubscription(subscription: ADJSubscription): void;
-  removeSessionCallbackParameter(key: string): void;
-  resetSessionCallbackParameters(): void;
-  addSessionPartnerParameterValue(key: string, value: string): void;
-  removeSessionPartnerParameter(key: string): void;
-  resetSessionPartnerParameters(): void;
-  gdprForgetMe(): void;
-  disableThirdPartySharing(): void;
-  updateConversionValue(conversionValue: number) : void;
-  appTrackingAuthorizationStatus(): void;
-  idfa: string | null;
-  adid: string | null;
-  attribution: NSMutableDictionary;
-  static sdkVersion: number;
-  convertUniversalLinkScheme(url: NSURL, scheme: string): void;
-  trackThirdPartySharing(adjustThirdPartySharing: AdjustThirdPartySharing): void;
-  trackMeasurementConsent(measurementConsent: boolean): void;
+	static new(): Adjust; // inherited from NSObject
+	static alloc(): Adjust; // inherited from NSObject
+	appDidLaunch(config: ADJConfig): void;
+	requestTrackingAuthorizationWithCompletionHandler(callback: any): void;
+	addSessionCallbackParameterValue(key: string, value: string): void;
+	trackSubsessionStart(): void;
+	trackEvent(event: ADJEvent): void;
+	appWillOpenUrl(url: NSURL): void;
+	sendFirstPackages(): void;
+	setOfflineMode(isEnabled: boolean): void;
+	setEnabled(isEnabled: boolean): void;
+	trackAdRevenuePayload(source: string, payload: NSData): void;
+	trackSubscription(subscription: ADJSubscription): void;
+	removeSessionCallbackParameter(key: string): void;
+	resetSessionCallbackParameters(): void;
+	addSessionPartnerParameterValue(key: string, value: string): void;
+	removeSessionPartnerParameter(key: string): void;
+	resetSessionPartnerParameters(): void;
+	gdprForgetMe(): void;
+	disableThirdPartySharing(): void;
+	updateConversionValue(conversionValue: number): void;
+	appTrackingAuthorizationStatus(): void;
+	idfa: string | null;
+	adid: string | null;
+	attribution: NSMutableDictionary;
+	static sdkVersion: number;
+	convertUniversalLinkScheme(url: NSURL, scheme: string): void;
+	trackThirdPartySharing(adjustThirdPartySharing: AdjustThirdPartySharing): void;
+	trackMeasurementConsent(measurementConsent: boolean): void;
 }
 
 declare class ADJEvent {
-  static new(): ADJEvent; // inherited from NSObject
-  initWithEventToken(token: string): void;
-  setRevenueCurrency(revenue, currency): void;
+	static new(): ADJEvent; // inherited from NSObject
+	initWithEventToken(token: string): void;
+	setRevenueCurrency(revenue, currency): void;
 }
 
 declare var ADJEnvironmentSandbox: string;
@@ -55,16 +56,16 @@ declare var ADJLogLevelAssert: string;
 declare var ADJLogLevelSuppress: string;
 
 declare class AdjustDelegate {
-  adjustDeeplinkResponse(url: URL): void;
+	adjustDeeplinkResponse(url: URL): void;
 }
 
 declare class ADJSubscription {
-  static alloc(): ADJSubscription;
-  initWithPriceCurrencyTransactionIdAndReceipt(decimalPrice: NSDecimalNumber, currency: string, transactionId: string, receiptValue: NSUTF8StringEncoding): ADJSubscription;
-  salesRegion: string;
+	static alloc(): ADJSubscription;
+	initWithPriceCurrencyTransactionIdAndReceipt(decimalPrice: NSDecimalNumber, currency: string, transactionId: string, receiptValue: NSUTF8StringEncoding): ADJSubscription;
+	salesRegion: string;
 }
 
 declare class AdjustThirdPartySharing {
-  static alloc(): AdjustThirdPartySharing;
-  initWithIsEnabledNumberBool(isEnabled: boolean): AdjustThirdPartySharing;
+	static alloc(): AdjustThirdPartySharing;
+	initWithIsEnabledNumberBool(isEnabled: boolean): AdjustThirdPartySharing;
 }
--- packages/adjust/adjust.ios.ts
+++ packages/adjust/adjust.ios.ts
@@ -1,61 +1,62 @@
 const adjustClient = Adjust.new();
 let callback;
 
-adjustClient.requestTrackingAuthorizationWithCompletionHandler(status => {
-  console.log(status);
+adjustClient.requestTrackingAuthorizationWithCompletionHandler((status) => {
+	console.log(status);
 
-  if(callback) {
-    callback(status);
-  }
+	if (callback) {
+		callback(status);
+	}
 });
 export const adjust = {
-    create: function(config) {
-      adjustClient.appDidLaunch(config);
-    },
-    requestTrackingAuthorizationWithCompletionHandler: fn => {
-      callback = fn
-    },
-    addSessionCallbackParameter: (key: string, value: string) => adjustClient.addSessionCallbackParameterValue(key, value),
-    trackSubsessionStart: () => adjustClient.trackSubsessionStart(),
-    trackEvent: event => adjustClient.trackEvent(event),
-    appWillOpenUrl: url => adjustClient.appWillOpenUrl(NSURL.URLWithString(url)),
-    getAmazonAdId: callback => callback(""),
-    getGoogleAdId: callback => callback(""),
-    sendFirstPackages: () => adjustClient.sendFirstPackages(),
-    setOfflineMode: (enabled: boolean) => adjustClient.setOfflineMode(enabled),
-    trackAdRevenue: (source: string, payload: string) => {
-      const text = NSString.stringWithString(payload);
-      const data: NSData = text.dataUsingEncoding(NSUTF8StringEncoding);
-      adjustClient.trackAdRevenuePayload(source, data);
-    },
-    trackAppStoreSubscription: (subscription: ADJSubscription) => adjustClient.trackSubscription(subscription),
-    removeSessionCallbackParameter: (key: string) => adjustClient.removeSessionCallbackParameter(key),
-    resetSessionCallbackParameters: () => adjustClient.resetSessionCallbackParameters(),
-    addSessionPartnerParameter: (key: string, value: string) => adjustClient.addSessionPartnerParameterValue(key, value),
-    removeSessionPartnerParameter: (key: string) => adjustClient.removeSessionPartnerParameter(key),
-    resetSessionPartnerParameters: () => adjustClient.resetSessionPartnerParameters(),
-    gdprForgetMe: () => adjustClient.gdprForgetMe(),
-    disableThirdPartySharing: () => adjustClient.disableThirdPartySharing(),
-    updateConversionValue: (conversionValue: number) => adjustClient.updateConversionValue(conversionValue),
-    getAppTrackingAuthorizationStatus: (callback) => callback(adjustClient.appTrackingAuthorizationStatus()),
-    getIdfa: (callback) => callback(adjustClient.idfa),
-    getAdid: (callback) => callback(adjustClient.adid),
-    getSdkVersion: (sdkPrefix: string, callback) => {
-      const sdkVersion = Adjust.sdkVersion;
+	create: function (config) {
+		adjustClient.appDidLaunch(config);
+	},
+	requestTrackingAuthorizationWithCompletionHandler: (fn) => {
+		callback = fn;
+	},
+	addSessionCallbackParameter: (key: string, value: string) => adjustClient.addSessionCallbackParameterValue(key, value),
+	trackSubsessionStart: () => adjustClient.trackSubsessionStart(),
+	trackEvent: (event) => adjustClient.trackEvent(event),
+	appWillOpenUrl: (url) => adjustClient.appWillOpenUrl(NSURL.URLWithString(url)),
+	getAmazonAdId: (callback) => callback(''),
+	getGoogleAdId: (callback) => callback(''),
+	sendFirstPackages: () => adjustClient.sendFirstPackages(),
+	setOfflineMode: (enabled: boolean) => adjustClient.setOfflineMode(enabled),
+	setEnabled: (enabled: boolean) => adjustClient.setEnabled(enabled),
+	trackAdRevenue: (source: string, payload: string) => {
+		const text = NSString.stringWithString(payload);
+		const data: NSData = text.dataUsingEncoding(NSUTF8StringEncoding);
+		adjustClient.trackAdRevenuePayload(source, data);
+	},
+	trackAppStoreSubscription: (subscription: ADJSubscription) => adjustClient.trackSubscription(subscription),
+	removeSessionCallbackParameter: (key: string) => adjustClient.removeSessionCallbackParameter(key),
+	resetSessionCallbackParameters: () => adjustClient.resetSessionCallbackParameters(),
+	addSessionPartnerParameter: (key: string, value: string) => adjustClient.addSessionPartnerParameterValue(key, value),
+	removeSessionPartnerParameter: (key: string) => adjustClient.removeSessionPartnerParameter(key),
+	resetSessionPartnerParameters: () => adjustClient.resetSessionPartnerParameters(),
+	gdprForgetMe: () => adjustClient.gdprForgetMe(),
+	disableThirdPartySharing: () => adjustClient.disableThirdPartySharing(),
+	updateConversionValue: (conversionValue: number) => adjustClient.updateConversionValue(conversionValue),
+	getAppTrackingAuthorizationStatus: (callback) => callback(adjustClient.appTrackingAuthorizationStatus()),
+	getIdfa: (callback) => callback(adjustClient.idfa),
+	getAdid: (callback) => callback(adjustClient.adid),
+	getSdkVersion: (sdkPrefix: string, callback) => {
+		const sdkVersion = Adjust.sdkVersion;
 
-      if(!sdkVersion) {
-        callback("");
-      } else {
-        callback(`${sdkPrefix}@${sdkVersion}`);
-      }
-    },
-    setReferrer: () => {},
-    trackPlayStoreSubscription: () => {},
-    getAttribution: (callback) => callback(adjustClient.attribution),
-    convertUniversalLink: (urlString: string, scheme: string, callback) => {
-      const url = NSURL.alloc().initWithString(urlString);
-      callback(adjustClient.convertUniversalLinkScheme(url, scheme));
-    },
-    trackThirdPartySharing: (adjustThirdPartySharing: AdjustThirdPartySharing) => adjustClient.trackThirdPartySharing(adjustThirdPartySharing),
-    trackMeasurementConsent: (measurementConsent: boolean) => adjustClient.trackMeasurementConsent(measurementConsent)
-  };
+		if (!sdkVersion) {
+			callback('');
+		} else {
+			callback(`${sdkPrefix}@${sdkVersion}`);
+		}
+	},
+	setReferrer: () => {},
+	trackPlayStoreSubscription: () => {},
+	getAttribution: (callback) => callback(adjustClient.attribution),
+	convertUniversalLink: (urlString: string, scheme: string, callback) => {
+		const url = NSURL.alloc().initWithString(urlString);
+		callback(adjustClient.convertUniversalLinkScheme(url, scheme));
+	},
+	trackThirdPartySharing: (adjustThirdPartySharing: AdjustThirdPartySharing) => adjustClient.trackThirdPartySharing(adjustThirdPartySharing),
+	trackMeasurementConsent: (measurementConsent: boolean) => adjustClient.trackMeasurementConsent(measurementConsent),
+};
